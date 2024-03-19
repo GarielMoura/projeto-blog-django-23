2aac6cfdc22aff170cc08a5879201b6609d4dd8f
Gabriel Moura <gabrielgs13@outlook.com>
2024-03-19 10:38:09 -0300

    30° alteracao

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -25,12 +25,18 @@ class SiteSetup(models.Model):
 
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=255)
+
     show_header = models.BooleanField(default=True)
     show_search = models.BooleanField(default=True)
     show_menu = models.BooleanField(default=True)
     show_description = models.BooleanField(default=True)
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
+
+    favicon = models.ImageField(
+        upload_to='assets/favicon/%Y/%m/',
+        blank=True, default=''
+    )
 
     def __str__(self):
         return self.title
