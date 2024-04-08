7381dc6eae469eff2318f9a0112ab301fb95af87
Gabriel Moura <gabrielgs13@outlook.com>
2024-04-08 08:23:13 -0300

    48° alteracao

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from utils.rands import slugfy_new
+from utils.images import resize_image
 
 
 class Tag(models.Model):
@@ -119,4 +120,15 @@ class Post(models.Model):
     def save(self, *args, **kwargs):  # noqa: F811
         if not self.slug:
             self.slug = slugfy_new(self.title, 4)
-        return super().save(*args, **kwargs)
+
+        current_cover_name = str(self.cover.name)
+        super_save = super().save(*args, **kwargs)
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover.name
+
+        if cover_changed:
+            resize_image(self.cover, 900, True, 70)
+
+        return super_save
