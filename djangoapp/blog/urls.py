cea9207fc337f6837f225076e6346cccd0854daf
Gabriel Moura <gabrielgs13@outlook.com>
2024-05-05 10:59:50 -0300

    67° alteracao

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,8 +1,8 @@
 # type: ignore
 from django.urls import path
 from blog.views import \
-    post, \
-    page, \
+    PostDetailView, \
+    PageDetailView, \
     CreatedByListView, \
     CategoryListView, \
     TagListView, \
@@ -13,8 +13,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
-    path('post/<slug:slug>/', post, name='post'),
-    path('page/<slug:slug>/', page, name='page'),
+    path('post/<slug:slug>/', PostDetailView.as_view(), name='post'),
+    path('page/<slug:slug>/', PageDetailView.as_view(), name='page'),
     path('created_by/<int:author_pk>/',
          CreatedByListView.as_view(), name='created_by'),
     path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
